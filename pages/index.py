922622661db3e67c94ee3e2ed60e185bab085172
Brian Ross <48453510+BrianThomasRoss@users.noreply.github.com>
2019-11-25 10:44:04 -0500

    Update Homepage
    Updates content of homepage, adds mapbox scatter plot.

--- pages/index.py
+++ pages/index.py
@@ -14,26 +14,30 @@ from app import app
 column1 = dbc.Col(
     [
         dcc.Markdown(
-            """
-        
-            ## Your Value Proposition
+            """        
+            # **Force Projection**. 
+            ## *Simplified*.
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
+            ##### Now more than ever police departments across the country face expanding and dynamic challenges. From traditional policing, to new spectres like counter-terrorism, the requirements of a police force are evolving. Add to this the current reality of budget constraints and these concerns become even more difficult to manage.
 
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
-
+            Thankfully technology is evolving alongside your department's law enforcement demands. Combining state of the art predictive modelling techniques with modern computing power
+             your department can draw on its existing wealth of data to build a comprehensive picture of where the demands are and where they will come from next.
+             Allowing your department to shift it's focus from logistics and analysis to maintaining **law and order**. 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Behind The Curtain', color='primary'), href='/predictions')
     ],
     md=4,
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
+data = pd.read_csv('assets\raw_csvs\scatter_data.csv')
+
+token = ('pk.eyJ1IjoiYnJpYW50aG9tYXNyb3NzIiwiYSI6ImNrMzY5ZTFyeDFvbm0zbXBwcGU4eW9wZWYifQ.BdRmQ9Q7siK7XNnFTvuasQ')
+
+fig = px.scatter_mapbox(data, lat = 'latitude', lon = 'longitude', hover_name='calldescription',zoom=10.25)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(mapbox_style="dark", mapbox_accesstoken = token)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 column2 = dbc.Col(
     [
