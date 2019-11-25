7f175caef82edcc112fb8717063d3abf819ef5c1
Brian Ross <48453510+BrianThomasRoss@users.noreply.github.com>
2019-11-25 10:49:21 -0500

    Add pandas

--- pages/index.py
+++ pages/index.py
@@ -5,6 +5,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import pandas as pd
 
 # Imports from this application
 from app import app
