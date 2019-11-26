933a309c45f8fad77511090c85d277e6082d7581
Brian Ross <48453510+BrianThomasRoss@users.noreply.github.com>
2019-11-26 11:44:07 -0500

    Update predictions.py

--- pages/predictions.py
+++ pages/predictions.py
@@ -35,7 +35,13 @@ column1 = dbc.Col(
 df = pd.read_csv('assets/raw-csvs/scatter_data.csv')
 
 fig = go.Figure(go.Densitymapbox(lat=df.latitude, lon=df.longitude, radius=5))
-fig.update_layout(mapbox_style="dark", mapbox_accesstoken = token)
+fig.update_layout(
+    mapbox = {
+        'accesstoken': token,
+        'style': "dark"},
+    showlegend = False,
+    mapbox_center_lon =-83,
+    mapbox_center_lat =42.4)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 
