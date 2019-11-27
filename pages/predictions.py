0ac2db1e6ab498b0ff941cefab88782ea278df37
Brian Ross <48453510+BrianThomasRoss@users.noreply.github.com>
2019-11-27 02:05:48 -0500

    Add Prediction Functionality
    Hopefully

--- pages/predictions.py
+++ pages/predictions.py
@@ -13,10 +13,11 @@ from joblib import load
 from app import app
 
 #
-# model = load('assets/pipeline.joblib')
+model = load('assets/model.joblib')
 token = ('pk.eyJ1IjoiYnJpYW50aG9tYXNyb3NzIiwiYSI6ImNrMzY5ZTFyeDFvbm0zbXBwcGU4eW9wZWYifQ.BdRmQ9Q7siK7XNnFTvuasQ')
 
 weather_options = ['Clear', 'Light Rain', 'Heavy Rain', ' Thunderstorm', 'Light Snow', 'Heavy Snow']
+condition_codes = [800, 500, 502, 202, 600, 602]
 text = {'display':'inline-block','textAlign': 'center'}
 
 # 2 column layout. 1st column width = 4/12
@@ -42,7 +43,13 @@ column1 = dbc.Col(
         html.Br(),
         dcc.Dropdown(
             id = 'weather_dropdown',
-            options = [{'label':option,'value':option} for option in weather_options],
+            options = [
+                {'label':'Clear','value':800},
+                {'label': 'Light Rain', 'value': 500},
+                {'label': 'Heavy Rain', 'value': 502},
+                {'label': 'Thunderstorm', 'value': 202},
+                {'label': 'Light Snow', 'value': 600},
+                {'label': 'Heavy Snow', 'value': 602}],
             placeholder = 'Conditions'
         ),
         
@@ -79,7 +86,6 @@ column1 = dbc.Col(
         labelStyle = {
         'display': 'inline-block',
         'margin-right': 30},
-        value='No'
         ),
 
         html.H3('Holiday Or Event', style = text),
@@ -93,11 +99,10 @@ column1 = dbc.Col(
         labelStyle = {
         'display': 'inline-block',
         'margin-right': 30},
-        value='No'
         ),
         
         html.Hr(),
-        html.Button('Submit', id='click', style = {'margin-left': 125})
+        html.Button('Submit', id='click', style = {'margin-left': 150})
 
     ],
     md=4,
@@ -116,6 +121,7 @@ fig.update_layout(
     mapbox_center_lon =-83.09587,
     mapbox_center_lat =42.3425, 
     height = 650,
+    width  = 800
     )
 
 
@@ -123,9 +129,75 @@ fig.update_layout(
 
 column2 = dbc.Col(
     [
-    dcc.Graph(id='graph', figure=fig),
+    dcc.Graph(id='prediction-graph', figure=fig),
     ]
     ,md=8)
+
+
+@app.callback(
+    Output('prediction-graph', 'figure'),
+    [Input('datepicker', 'date'),
+     Input('weather_dropdown', 'condition'),
+     Input('low_temp', 'low'),
+     Input('high_temp', 'high'),
+     Input('severe_flag', 'severe'),
+     Input('holiday_flag', 'holiday')])
+
+
+def update_pred(date, condition, low_temp, high_temp, severe, holiday):
+    date = pd.to_datetime(date)
+    year = date.year
+    month = date.month
+    week = date.week
+    dow = date.dayofweek
+    day = date.day
+
+    df = pd.read_csv('assets/raw-csvs/pred_template.csv')
+    df = df.drop(columns='Unnamed: 0')
+    length = len(df)
+
+    df['year'] = [year]*length
+    df['month'] = [month]*length
+    df['day'] = [day]*length
+    df['week'] = [week]*length
+    df['dow'] = [dow]*length
+
+    lat_max =  42.46
+    lon_max = -82.91
+    lat_min =  42.25
+    lon_min = -83.28
+
+    lon_range = lon_max - lon_min
+    lat_range = lat_max - lat_min
+
+    lat_length = lat_range / 10
+    lon_length = lon_range / 15
+
+    preds = pipeline.predict(df)
+
+    df['count'] = preds
+    
+    df['lat_center'] = lat_min + ((lat_length * df['lat_grid']) + (.5*lat_length))
+    df['lon_center'] = lon_min - ((lon_length * df['lon_grid']) + (.5*lon_length))
+
+    fig = go.Figure(go.Densitymapbox(lat=df.lat_center, lon=df.lon_center, radius=5))
+    fig.update_layout(
+        mapbox = {
+            'accesstoken': token,
+            'style': "dark",
+            'zoom': 10},
+        margin={"r":0,"t":0,"l":0,"b":0},
+        showlegend = False,
+        mapbox_center_lon =-83.09587,
+        mapbox_center_lat =42.3425, 
+        height = 650,
+        width  = 800
+    )
+
+    return fig
+
+fig = update_pred(date, condition, low_temp, high_temp, severe, holiday)
+
 
 
 
