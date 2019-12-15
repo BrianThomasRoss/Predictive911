ccc10f5cc12d489d20f5d9053439c7e5d102d2f3
brianthomasross <brianthomasross@users.noreply.github.com>
2019-12-15 01:54:35 -0500

    Update predictions.py

--- pages/predictions.py
+++ pages/predictions.py
@@ -181,11 +181,10 @@ def update_pred(date, condition, low, high, severe, holiday):
     df['is_holiday'] = [holiday]*length
 
     # Weather
-    condition = condition.astype(int)
 
     df['temp_min'] = [65]*length
     df['temp_max'] = [70]*length
-    df['weather_id'] = [condition]*length
+    df['weather_id'] = [600]*length
     df['is_severe'] = [severe]*length
 
     lat_max =  42.46
